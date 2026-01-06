fcfd865de888a8349725b155f0e939dfaec11d32
nazmulfahim000 <nazmulfahim000@gmail.com>
2026-01-06 12:57:38 +0600

    write on display and distance

--- all_marks.py
+++ all_marks.py
@@ -21,6 +21,7 @@ def main():
         landmarker = HandLandmarker.create_from_options(options)
 
         cam = cv2.VideoCapture(0)
+        
         while cam.isOpened:
             success , frame = cam.read()
 
@@ -33,57 +34,49 @@ def main():
             timestamp_ms = int(cam.get(cv2.CAP_PROP_POS_MSEC))
             result = landmarker.detect_for_video(mp_image,timestamp_ms)
             
-        
+            HAND_CONNECTIONS = [
+                (0, 1), (1, 2), (2, 3), (3, 4),(4,8),    # Thumb
+                (0, 5), (5, 6), (6, 7), (7, 8),    # Index Finger
+                (5, 9), (9, 10), (10, 11), (11, 12), # Middle Finger
+                (9, 13), (13, 14), (14, 15), (15, 16), # Ring Finger
+                (13, 17), (17, 18), (18, 19), (19, 20), (0, 17) # Pinky and Palm
+            ]
+
             if result.hand_landmarks:
                 for hand_idx, hand_landmarks in enumerate(result.hand_landmarks):
                     print(f"----------{hand_idx+1}-----------")
 
-                    line_data={}
-
-                    for point, land_mark in enumerate(hand_landmarks):
+                    point=[]
+                    for pos, land_mark in enumerate(hand_landmarks):
                         h, w, _ = frame.shape
                         # print(point)
-                        if point==8 or point == 4 or point == 0:
-                            print(point)
-                            x_pixel,y_pixel, z_value = int(land_mark.x*w),int(land_mark.y*h),int(land_mark.z)
+                        point.append((int(land_mark.x*w),int(land_mark.y*h))) 
 
-                            line_data[point]=(x_pixel,y_pixel)
-
-                            print(f'-->{point}  x:{x_pixel} y:{y_pixel}')
-                            cv2.circle(frame,(x_pixel,y_pixel),5,(0,0,255),-1)
+                        # print(f'-->{pos}  x:{x_pixel} y:{y_pixel}')
+                        # cv2.circle(frame,(x_pixel,y_pixel),5,(0,0,255),-1)
                             
+                    for connection in HAND_CONNECTIONS:
+                            str_ind=connection[0]
+                            end_ind=connection[1]
 
-                    if len(line_data):
-                        p8=line_data[8]
-                        p4=line_data[4]
-                    
-
-                        distance=((p8[0]-p4[0])**2+(p8[1]-p4[1])**2)**0.5
-                        cv2.line(frame,p4,p8,(0,155,0),2)
-                        print(int(distance))
-                        cv2.putText(frame, f"Dist: {int(distance)}", (50, 50), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-                        # if distance <= 30 and distance>=10:
-                        #     cam.release()
-                        #     cv2.destroyAllWindows()
-                        if distance < 30:
-                            cv2.putText(frame, "Halland!", (200, 200), 
-                            cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
-
+                            cv2.line(frame,point[str_ind],point[end_ind],(0,255,0),2)
+                        
+                    for cir in point:
+                            cv2.circle(frame,cir,5,(0,0,255),-1)
             cv2.imshow('window',frame)
             # cv2.imshow('RGB',frameRGB)
             # print(result.hand_landmarks)
-            
+
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
+       
         cam.release()
-        cv2.destroyAllWindows()
-
 
     
 
 
 if __name__ == '__main__':
     main()
+    
+    cv2.destroyAllWindows()
